1f814f5e68190731e7e66378343a20c004ef6f5f
Tanakorn Phoochaliaw <tanakorn.ph@castcle.com>
2021-08-14 22:20:17 +0700

    Init project

---
+++ Post/Post.h
@@ -0,0 +1,18 @@
+//
+//  Post.h
+//  Post
+//
+//  Created by Tanakorn Phoochaliaw on 14/8/2564 BE.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Post.
+FOUNDATION_EXPORT double PostVersionNumber;
+
+//! Project version string for Post.
+FOUNDATION_EXPORT const unsigned char PostVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Post/PublicHeader.h>
+
+
