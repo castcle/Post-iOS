906cc361268f2d08a07b0cbb2a261b544f326d91
Tanakorn Phoochaliaw <tanakorn.ph@castcle.com>
2021-09-01 19:05:05 +0700

    Update font icon

--- Post/ImagePostTableViewCell.swift
+++ Post/ImagePostTableViewCell.swift
@@ -69,54 +69,50 @@ class ImagePostTableViewCell: UITableViewCell {
     @IBOutlet var containerConstraint: NSLayoutConstraint!
     
     var delegate: ImagePostTableViewCellDelegate?
-    var multiplyImage: UIImage {
-        let symbolConfiguration = UIImage.SymbolConfiguration(pointSize: 16, weight: .medium)
-        let homeImage = UIImage(systemName: "multiply", withConfiguration: symbolConfiguration) ?? UIImage()
-        return homeImage
-    }
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        let incorrectImage = UIImage.init(icon: .castcle(.incorrect), size: CGSize(width: 20, height: 20), textColor: UIColor.Asset.white)
         
         self.containerView.custom(cornerRadius: 12)
         self.button11.setBackgroundImage(UIColor.Asset.gray.toImage()?.alpha(0.4), for: .normal)
-        self.button11.setImage(self.multiplyImage, for: .normal)
+        self.button11.setImage(incorrectImage, for: .normal)
         self.button11.tintColor = UIColor.Asset.white
         self.button11.capsule()
         self.button21.setBackgroundImage(UIColor.Asset.gray.toImage()?.alpha(0.4), for: .normal)
-        self.button21.setImage(self.multiplyImage, for: .normal)
+        self.button21.setImage(incorrectImage, for: .normal)
         self.button21.tintColor = UIColor.Asset.white
         self.button21.capsule()
         self.button22.setBackgroundImage(UIColor.Asset.gray.toImage()?.alpha(0.4), for: .normal)
-        self.button22.setImage(self.multiplyImage, for: .normal)
+        self.button22.setImage(incorrectImage, for: .normal)
         self.button22.tintColor = UIColor.Asset.white
         self.button22.capsule()
         self.button31.setBackgroundImage(UIColor.Asset.gray.toImage()?.alpha(0.4), for: .normal)
-        self.button31.setImage(self.multiplyImage, for: .normal)
+        self.button31.setImage(incorrectImage, for: .normal)
         self.button31.tintColor = UIColor.Asset.white
         self.button31.capsule()
         self.button32.setBackgroundImage(UIColor.Asset.gray.toImage()?.alpha(0.4), for: .normal)
-        self.button32.setImage(self.multiplyImage, for: .normal)
+        self.button32.setImage(incorrectImage, for: .normal)
         self.button32.tintColor = UIColor.Asset.white
         self.button32.capsule()
         self.button33.setBackgroundImage(UIColor.Asset.gray.toImage()?.alpha(0.4), for: .normal)
-        self.button33.setImage(self.multiplyImage, for: .normal)
+        self.button33.setImage(incorrectImage, for: .normal)
         self.button33.tintColor = UIColor.Asset.white
         self.button33.capsule()
         self.button41.setBackgroundImage(UIColor.Asset.gray.toImage()?.alpha(0.4), for: .normal)
-        self.button41.setImage(self.multiplyImage, for: .normal)
+        self.button41.setImage(incorrectImage, for: .normal)
         self.button41.tintColor = UIColor.Asset.white
         self.button41.capsule()
         self.button42.setBackgroundImage(UIColor.Asset.gray.toImage()?.alpha(0.4), for: .normal)
-        self.button42.setImage(self.multiplyImage, for: .normal)
+        self.button42.setImage(incorrectImage, for: .normal)
         self.button42.tintColor = UIColor.Asset.white
         self.button42.capsule()
         self.button43.setBackgroundImage(UIColor.Asset.gray.toImage()?.alpha(0.4), for: .normal)
-        self.button43.setImage(self.multiplyImage, for: .normal)
+        self.button43.setImage(incorrectImage, for: .normal)
         self.button43.tintColor = UIColor.Asset.white
         self.button43.capsule()
         self.button44.setBackgroundImage(UIColor.Asset.gray.toImage()?.alpha(0.4), for: .normal)
-        self.button44.setImage(self.multiplyImage, for: .normal)
+        self.button44.setImage(incorrectImage, for: .normal)
         self.button44.tintColor = UIColor.Asset.white
         self.button44.capsule()
     }
