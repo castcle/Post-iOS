b4413c4d498f3455c58cb006a544afaa1b0794e6
Tanakorn Phoochaliaw <tanakorn.ph@castcle.com>
2022-03-10 01:29:59 +0700

    Remove comment line

--- Post/Cell/Post/ImagePostTableViewCell.swift
+++ Post/Cell/Post/ImagePostTableViewCell.swift
@@ -119,8 +119,6 @@ class ImagePostTableViewCell: UITableViewCell {
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
     
     func configCell(image: [TLPHAsset]) {
