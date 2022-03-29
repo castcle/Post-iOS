ac721d33e2419f3e58c9b314e876cb532dc51246
Tanakorn Phoochaliaw <tanakorn.dev@gmail.com>
2022-03-29 17:18:53 +0700

    Fix language display

--- Post/Cell/Post/HashtagTableViewCell.swift
+++ Post/Cell/Post/HashtagTableViewCell.swift
@@ -42,5 +42,4 @@ class HashtagTableViewCell: DropDownCell {
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
     }
-    
 }
